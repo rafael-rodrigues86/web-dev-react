39385375bcd3b64d005e93ea836d0db5fbdeb8d4
Rafael Rodrigues <rafael.raraujo86@gmail.com>
2023-10-20 17:34:20 -0300

    Add metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,23 @@
-"use client" 
-import { ThemeProvider } from '@/app/contexts/ThemeContext'
-import './globals.css'
-import { Inter } from 'next/font/google'
+"use client";
+import { ThemeProvider } from "@/app/contexts/ThemeContext";
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
-export default function RootLayout({ children  }) {
-  
+export const metadata = {
+  title: "Infnet E-commerce",
+  description: "Web Page for Infnet E-commerce",
+  keywords: "web development, web design, React, JavaScript, NextJS",
+};
+
+export default function RootLayout({ children }) {
   return (
     <ThemeProvider>
       <html lang="en">
-          <link rel="manifest" href="manifest.json"></link>
-          <body className={inter.className}>{children}</body>
+        <link rel="manifest" href="manifest.json"></link>
+        <body className={inter.className}>{children}</body>
       </html>
     </ThemeProvider>
-  )
+  );
 }
